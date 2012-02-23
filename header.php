7e3126d7a88a672033b0190d8f24e1fc9a04f115
Jessica White <jessplusplus@gmail.com>
2012-02-23 14:40:36 -0500

    updated index.php and added header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+
+<div id="header">
+		<div id="site-name">Drink Recipes</div>
+		<div id="search">
+		</div>
+		<ul id="nav">
+		<li class="active"><a href="index.html">Home</a></li>
+		<li><a href="register.php">Register</a>
+		</li>
+		<li><a href="login.php">Login</a>
+		</li>
+		<li><a href="search.php">Search</a>
+		</li>
+		</ul>
+	</div>
