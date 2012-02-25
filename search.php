fca4534236cb61179089304084fc849b2541904a
Chris Joyner <chrisjoyner2005@yahoo.com>
2012-02-25 15:15:02 -0500

    Added code for if search item is not found

--- search.php
+++ search.php
@@ -1,28 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+Copyright: Daemon Pty Limited 2006, http://www.daemon.com.au
+Community: Mollio http://www.mollio.org $
+License: Released Under the "Common Public License 1.0", 
+http://www.opensource.org/licenses/cpl.php
+License: Released Under the "Creative Commons License", 
+http://creativecommons.org/licenses/by/2.5/
+License: Released Under the "GNU Creative Commons License", 
+http://creativecommons.org/licenses/GPL/2.0/
+-->
+ 
+	
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+   include('index.php');
+?>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>Search</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+  
 </head>
 
 <body>
-<div id="contents">
+<div>
   <h1>Search</h1>
   <form method="post" action="search.php">
-    <label for="username">Search:</label>
-    <input type="text" id="search" name="search" />
+	<label for="username">Search:</label>
+    <input type="text" id="search1" name="search1" />
     <input type="submit" value="go" name="submit" />
-  </form>
+  
   
   <?php
   include('db_connect.php');
   
   
-  if (isset($_POST['search']))
+  if (isset($_POST['search1']))
   {
-  	$searchterm = mysqli_real_escape_string($db, trim($_POST['search']));
+  	$searchterm = mysqli_real_escape_string($db, trim($_POST['search1']));
 
   	
   		$query = "SELECT mix_drinks.drink_name, ingredients.ingredient, strength.strength, difficulty.difficulty FROM 
@@ -35,7 +49,7 @@
   
   		$result = mysqli_query($db, $query)
    			or die("Error Querying Database");
-   		echo "<table id=\"hor-minimalist-b\">\n<tr><th>Drink Name</th><th>Strength</th><th>Difficulty</th><th>Ingredient</th><tr>\n\n";
+   		
 		
 		$name1 = "";
 		$count = 0;
@@ -45,8 +59,12 @@
 			
 			if ($count == 0)
 			{
+				echo "<table BORDER=4 CELLSPACING=4 CELLPADDING=4 id=\"hor-minimalist-b\">\n
+				<tr><th>Drink Name</th><th>Strength</th><th>Difficulty</th><th>Ingredient</th></tr>\n\n";
 				$strength = $row['strength'];
+				$strength = ucfirst($strength);
 				$difficulty = $row['difficulty'];
+				$difficulty = ucfirst($difficulty);
 				$ingredient = $row['ingredient'];
 				echo "<tr><td>$name</td><td >$strength</td><td>$difficulty</td><td >$ingredient";
 				$count++;
@@ -60,20 +78,30 @@
 			{
 				echo "</td></tr>\n";
 				$strength = $row['strength'];
+				$strength = ucfirst($strength);
 				$difficulty = $row['difficulty'];
+				$difficulty = ucfirst($difficulty);
 				$ingredient = $row['ingredient'];
 				echo "<tr><td>$name</td><td >$strength</td><td>$difficulty</td><td >$ingredient";
 			}
 			
 			$name1 = $row['drink_name'];
 	    }
-	    echo "</table>\n"; 
 		
+	   
+		
+		if ($count == 0)
+		{
+			
+			echo "<table><tr><td>Search item not found</td></tr>";
+		}
+		 echo "</table>\n"; 
   	}
   	
   
   ?>
   <p>&nbsp;</p><p><a href="logout.php">logout</a></p>
+  </form>
   </div>
 </body>
 </html>
