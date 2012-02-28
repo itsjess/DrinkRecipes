b8df8fa251fbd2042beff8632148eb0348a4e0a8
Kurt Wall <kurtwall92@gmail.com>
2012-02-28 13:55:35 -0500

    this is an about us page

---
+++ about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+Copyright: Daemon Pty Limited 2006, http://www.daemon.com.au
+Community: Mollio http://www.mollio.org $
+License: Released Under the "Common Public License 1.0", 
+http://www.opensource.org/licenses/cpl.php
+License: Released Under the "Creative Commons License", 
+http://creativecommons.org/licenses/by/2.5/
+License: Released Under the "GNU Creative Commons License", 
+http://creativecommons.org/licenses/GPL/2.0/
+-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Drink Recipes</title>
+<link rel="stylesheet" type="text/css" href="css/main.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="css/print.css" media="print" />
+<!--[if lte IE 6]>
+<link rel="stylesheet" type="text/css" href="css/ie6_or_less.css" />
+<![endif]-->
+<script type="text/javascript" src="js/common.js"></script>
+</head>
+<body id="type-a">
+<div id="wrap">
+<?php
+   include('header.php');
+?>
+	<!--<div id="header">
+		<div id="site-name">Drink Recipes</div>
+		<div id="search">
+			
+		</div>
+		<ul id="nav">
+		<li class="active"><a href="index.html">Home</a></li>
+		<li><a href="register.php">Register</a>
+			
+		</li>
+		<li><a href="login.php">Login</a>
+			
+		</li>
+		<li><a href="search.php">Search</a>
+			
+		</li>
+		</ul>
+	</div>-->
+	
+	<div id="content-wrap">
+	
+		<div id="content">
+					
+					<!-- CONTENT -->
+					<h3>About Us!</h3>
+					<p>Hello! This site is for people that have a passion for making mixed drinks. We wanted to make a site that made creating drinks an even more fun and rewarding experience. Not only will you enjoy mixing much more, but you can fine tune your mixing skills and keep track of your progress while being introduced to various drink options. We hope you like it!</p>
+					<!-- END CONTENT -->
+		
+		
+		</div>
+		
+		
+		
+	</div>
+
+</div>
+</body>
+</html>
