92a6f677b62d1a7e344aeeef6c088a909f4ba006
Chris Joyner <chrisjoyner2005@yahoo.com>
2012-03-19 15:31:51 -0400

    CookieCheck

---
+++ CookieCheck.php
@@ -0,0 +1,12 @@
+<?php
+
+// If the session vars aren't set, try to set them with a cookie
+  if (!isset($_SESSION['user_id'])){
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['user_name'])) {
+      $_SESSION['user_id'] = $_COOKIE['user_id'];
+      $_SESSION['user_name'] = $_COOKIE['user_name'];
+    }
+  }
+ 
+?>
+	
