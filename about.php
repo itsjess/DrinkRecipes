405d56cac4efd222c0dee93cb8583bf197264b39
JC Cheek <jadenray64@gmail.com>
2012-04-03 01:17:00 -0400

    added more spice JC

--- about.php
+++ about.php
@@ -51,12 +51,29 @@ http://creativecommons.org/licenses/GPL/2.0/
 					
 					<!-- CONTENT -->
 					<h1>About Us!</h1>
-					<p>Hello! This site is for people that have a passion for making mixed drinks. 
-					We wanted to make a site that made creating drinks an even more fun and rewarding experience. 
+<table width="200" border="0" align="right" cellpadding="5" cellspacing="0">
+<tr>
+<td align="right" valign="top"><br>
+<img src="images/pina.jpg" width="200" height="200"><br>
+</td>
+</tr>
+</table>					
+<p><h5>Welcome! This site is for people that have a passion for making mixed drinks. 
+					Our missions is to make a site that made creating drinks an even more fun and rewarding experience. 
 					Not only will you enjoy mixing much more, but you can fine tune your mixing skills and keep track 
-					of your progress while being introduced to various drink options. We hope you like it!
-					<img src="images/pina.jpg" />
-					</p>
+					of your progress while being introduced to various drink options. We hope you like it!</h5>
+					
+
+					 
+					<h1> What's New!</h1>
+					<h5> We are constantly updating and improving our site to better suite the needs of our visitors. If you have any suggestions of what you would like to see, feel free to provide feedback! In the meantime, check out some of these cool, new installations our top-notch engineers just came out with!</h5>
+
+<h3>Make An Account</h3>
+<h5>If you haven't already, you should go to DrinkRecipes/register.php and make your own account on Drink Recipes today! With an account on Drink Recipes, you can keep track of the drinks you have made, create a profile, and be kept up to date with the latest installations here at Drink Recipes. With future improvements planned, you can do even more with an account. Soon, you'll be able to keep track of drinks you favorite, look for drinks in your local area and meet people with similar tastes to yours! This is an opportunity I wouldn't miss out on.</h5>
+
+<h3>Add A Drink</h3>
+<h5>Our databases are now at your disposal for every concoction you can imagine. Put a personal spin on an old classic, share a family favorite, or crave about the local specialties. There are no limits to how many you can add - the more the merrier! You can add specific ingredients, names and even a picture to show others how it should (or shouldn't) look!</h5>
+
 					<!-- END CONTENT -->
 		
 		
